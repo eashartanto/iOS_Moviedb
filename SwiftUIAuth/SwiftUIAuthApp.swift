e9a35e7061d8e2b0d31fbc058adff3a4335448d4
heimlichit <eashartanto@outlook.com>
2023-04-03 17:41:55 -0400

    Initial Commit

---
+++ SwiftUIAuth/SwiftUIAuthApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAuthApp.swift
+//  SwiftUIAuth
+//
+//  Created by Andreas Hartanto on 2023-04-03.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAuthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
